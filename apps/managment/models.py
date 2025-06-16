ed05330c6ae22d4dfcbdc8792dd0870c3700b388
Ahmad Ali Khan Shirwany <shirwany.shirwany@gmail.com>
2025-06-17 02:18:28 +0300

    update

--- apps/managment/models.py
+++ apps/managment/models.py
@@ -428,6 +428,9 @@ class Product(models.Model):
 
         # Existing validation logic
         if self.harvest_date and self.entry_date:
+            today = timezone.now().date()
+            if harvest_date > today:
+                raise ValidationError("Harvest date cannot be in the future.")
             if self.harvest_date > self.entry_date:
                 raise ValidationError("Harvest date cannot be later than entry date.")
         if self.manufacturing_date and self.entry_date:
