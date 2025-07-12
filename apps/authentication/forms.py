634a7d8e26f47857e949f1486c8508f6e7dfd0ac
Ahmad Ali Khan Shirwany <shirwany.shirwany@gmail.com>
2025-07-12 16:59:10 +0300

    update

--- apps/authentication/forms.py
+++ apps/authentication/forms.py
@@ -70,28 +70,28 @@ class SignUpForm(UserCreationForm):
     username = forms.CharField(
         widget=forms.TextInput(
             attrs={
-                "placeholder": "Username",
+
                 "class": "form-control"
             }
         ))
     email = forms.EmailField(
         widget=forms.EmailInput(
             attrs={
-                "placeholder": "Email",
+
                 "class": "form-control"
             }
         ))
     password1 = forms.CharField(
         widget=forms.PasswordInput(
             attrs={
-                "placeholder": "Password",
+
                 "class": "form-control"
             }
         ))
     password2 = forms.CharField(
         widget=forms.PasswordInput(
             attrs={
-                "placeholder": "Password check",
+
                 "class": "form-control"
             }
         ))
